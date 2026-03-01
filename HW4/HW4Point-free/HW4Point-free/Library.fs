a1c1a045b1a53c4a4fef7d9518b3225e9b170883
bogdanovaarina <arinabogdanova1967@mail.ru>
2026-03-01 18:50:12 +0300

    add solution

---
+++ HW4/HW4Point-free/HW4Point-free/Library.fs
@@ -0,0 +1,7 @@
+﻿namespace HW4Point_free
+
+module Transformations =
+    let func x l= List.map (fun y -> y * x) l
+    let func'1 x= List.map (fun y -> y * x)
+    let func'2 x= List.map (fun y -> ((*)x) y)
+    let func'3 x= List.map ((*)x)
