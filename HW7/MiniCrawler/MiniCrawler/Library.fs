77e4948faf3f6e82573a093be6c49e8a5846bac5
bogdanovaarina <arinabogdanova1967@mail.ru>
2026-04-05 12:56:26 +0300

    rename file and add cap

--- HW7/MiniCrawler/MiniCrawler/Library.fs
+++ HW7/MiniCrawler/MiniCrawler/Library.fs
@@ -1,17 +1,23 @@
-﻿module MiniCrawler
+﻿// <copyright file="Library.fs" author="bogdanovaarina">
+// under MIT License
+// </copyright>
+
+module MiniCrawler
 
 open System
 open System.Net
 open System.Text.RegularExpressions
 
-let downloadPageAsync (url: string) = async {
-    use client = new WebClient()
-    let! html = client.AsyncDownloadString(Uri(url))
-    return (url, html.Length)
-}
+let downloadPageAsync (url: string) =
+    async {
+        use client = new WebClient()
+        let! html = client.AsyncDownloadString(Uri(url))
+        return (url, html.Length)
+    }
 
 let extractLinks (html: string) =
     let pattern = @"<a\s+href=[""'](http://[^""']*)[""']"
+
     Regex.Matches(html, pattern)
     |> Seq.cast<Match>
     |> Seq.map (fun m -> m.Groups[1].Value)
@@ -22,8 +28,5 @@ let miniCrawler (url: string) =
     use client = new WebClient()
     let mainHtml = client.DownloadString(Uri(url))
     let links = extractLinks mainHtml
-    
-    links
-    |> List.map downloadPageAsync
-    |> Async.Parallel
-    |> Async.RunSynchronously
+
+    links |> List.map downloadPageAsync |> Async.Parallel |> Async.RunSynchronously
