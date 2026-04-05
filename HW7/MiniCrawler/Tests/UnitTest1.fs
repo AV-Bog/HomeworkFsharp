ac8834f91adfd42672d5a7a3ccebec25f1ef4368
bogdanovaarina <arinabogdanova1967@mail.ru>
2026-04-05 12:25:45 +0300

    add solution

---
+++ HW7/MiniCrawler/Tests/UnitTest1.fs
@@ -0,0 +1,30 @@
+﻿module Tests
+
+open NUnit.Framework
+open MiniCrawler
+
+[<TestFixture>]
+type tests () =
+    [<Test>]
+    member this.``MiniCrawler_ReturnsArray`` () =
+        let results = miniCrawler "http://example.com"
+        Assert.That(results, Is.Not.Null)
+        Assert.That(results, Is.InstanceOf<(string * int)[]>())
+    
+    [<Test>]
+    member this.``MiniCrawler_EachPageHasPositiveSize`` () =
+        let results = miniCrawler "http://example.com"
+        for (url, size) in results do
+            Assert.That(size, Is.GreaterThan(0))
+    
+    [<Test>]
+    member this.``MiniCrawler_NoDuplicateUrls`` () =
+        let results = miniCrawler "http://example.com"
+        let urls = results |> Array.map fst
+        Assert.That(urls.Length, Is.EqualTo(urls |> Array.distinct |> Array.length))
+    
+    [<Test>]
+    member this.``MiniCrawler_InvalidUrl_ThrowsException`` () =
+        Assert.Throws<System.Net.WebException>(fun () ->
+            miniCrawler "http://this-site-does-not-exist-12345.com" |> ignore
+        ) |> ignore
