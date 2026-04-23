28820f5e6f3a8040e05552b8bff7ff9a39436aa0
bogdanovaarina <arinabogdanova1967@mail.ru>
2026-04-23 19:03:38 +0500

    add solution

---
+++ Test2/BlockingQueue/BlockingQueue/Library.fs
@@ -0,0 +1,40 @@
+﻿namespace BlockingQueue
+
+open System.Threading
+open System.Collections.Generic
+
+// Блокирующая очередь с потокобезопасным доступом
+type BlockingQueue<'T>() =
+    let queue = Queue<'T>()
+    let syncRoot = obj()
+    
+    // Добавить элемент в очередь
+    member this.Enqueue(item: 'T) =
+        lock syncRoot <| fun () ->
+            queue.Enqueue(item)
+            Monitor.Pulse(syncRoot)
+    
+    // Получить элемент из очереди (блокируется, если очередь пуста)
+    member this.Dequeue() =
+        lock syncRoot <| fun () ->
+            while queue.Count = 0 do
+                Monitor.Wait(syncRoot) |> ignore
+            queue.Dequeue()
+    
+    // Попытка получить элемент с таймаутом
+    member this.TryDequeue(timeoutMilliseconds: int) =
+        lock syncRoot <| fun () ->
+            if queue.Count = 0 then
+                Monitor.Wait(syncRoot, timeoutMilliseconds) |> ignore
+            if queue.Count > 0 then
+                Some(queue.Dequeue())
+            else
+                None
+    
+    // Получить текущий размер очереди
+    member this.Count =
+        lock syncRoot <| fun () -> queue.Count
+    
+    // Очистить очередь
+    member this.Clear() =
+        lock syncRoot <| fun () -> queue.Clear()
