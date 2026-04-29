a4748a5b326fcf9e9a5ee779d14e4aa65ae9c9b2
bogdanovaarina <arinabogdanova1967@mail.ru>
2026-04-29 21:57:30 +0300

    add solution

---
+++ HW8/Lazy/Lazy/Library.fs
@@ -0,0 +1,49 @@
+﻿namespace Lazy
+
+type ILazy<'a> =
+    abstract member Get: unit -> 'a
+
+type SimpleLazy<'a>(supplier : unit -> 'a) =
+    let mutable cached = None
+    
+    interface ILazy<'a> with
+        member this.Get() =
+            match cached with
+            | Some value -> value
+            | None ->
+                let value = supplier()
+                cached <- Some value
+                value
+
+type ThreadSafeLazy<'a>(supplier : unit -> 'a) =
+    let mutable cached = None
+    let lockObj = obj()
+    
+    interface ILazy<'a> with
+        member this.Get() =
+            match cached with
+            | Some value -> value
+            | None ->
+                lock lockObj (fun () ->
+                    match cached with
+                    | Some value -> value
+                    | None ->
+                        let value = supplier()
+                        cached <- Some value
+                        value
+                )
+
+type LockFreeLazy<'a>(supplier : unit -> 'a) =
+    let mutable cached = None
+    
+    interface ILazy<'a> with
+        member this.Get() =
+            match cached with
+            | Some value -> value
+            | None ->
+                let value = supplier()
+                match cached with
+                | Some existing -> existing
+                | None ->
+                    cached <- Some value
+                    value
