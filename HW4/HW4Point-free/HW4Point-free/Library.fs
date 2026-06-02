358ce1f3a710a774a0da24992ca9dbbe00d4dae6
bogdanovaarina <arinabogdanova1967@mail.ru>
2026-06-02 19:35:45 +0300

    fix point-free

--- HW4/HW4Point-free/HW4Point-free/Library.fs
+++ HW4/HW4Point-free/HW4Point-free/Library.fs
@@ -1,7 +1,8 @@
-﻿namespace HW4Point_free
+﻿namespace HW4PointFree
 
 module Transformations =
-    let func x l= List.map (fun y -> y * x) l
-    let func'1 x= List.map (fun y -> y * x)
-    let func'2 x= List.map (fun y -> ((*)x) y)
-    let func'3 x= List.map ((*)x)
+    let scaleList x l = List.map (fun y -> y * x) l
+    let scaleList1 x = List.map (fun y -> y * x)
+    let scaleList2 x = List.map (fun y -> ((*) x) y)
+    let scaleList3 x = List.map ((*) x)
+    let scaleListPF = List.map << (*)
---
+++ HW4/HW4Point-free/Tests/Tests.fs
@@ -0,0 +1,30 @@
+﻿module Tests
+
+open HW4PointFree.Transformations
+open NUnit.Framework
+
+[<TestFixture>]
+module FunctionTests =
+
+    let private isEqual f g (x: int) (lst: int list) =
+        f x lst = g x lst
+
+    [<FsCheck.NUnit.Property>]
+    let ``scaleList equals scaleList1`` (x: int, l: int list) =
+        scaleList x l = scaleList1 x l
+
+    [<FsCheck.NUnit.Property>]
+    let ``scaleList1 equals scaleList2`` (x: int, l: int list) =
+        scaleList1 x l = scaleList2 x l
+
+    [<FsCheck.NUnit.Property>]
+    let ``scaleList2 equals scaleList3`` (x: int, l: int list) =
+        scaleList2 x l = scaleList3 x l
+
+    [<FsCheck.NUnit.Property>]
+    let ``scaleList3 equals scaleListPF`` (x: int, l: int list) =
+        scaleList3 x l = scaleListPF x l
+
+    [<FsCheck.NUnit.Property>]
+    let ``scaleList equals scaleListPF (transitive)`` (x: int, l: int list) =
+        scaleList x l = scaleListPF x l
