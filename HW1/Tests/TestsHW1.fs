67ac900e8909d70993be764ad90acb0af449321a
bogdanovaarina <arinabogdanova1967@mail.ru>
2026-06-02 15:24:42 +0300

    исправление тестов для ряда степеней

--- HW1/Tests/TestsHW1.fs
+++ HW1/Tests/TestsHW1.fs
@@ -15,23 +15,27 @@ let ``Factorial of 20 should be large number`` () =
 
 [<Test>]
 let ``generatePowersOfTwo 0 0 should return single element`` () =
-    generatePowersOfTwo 0 0 |> should equal [1.0]
+    generatePowersOfTwo 0 0 |> should equal (Ok [1.0])
 
 [<Test>]
 let ``generatePowersOfTwo 0 5 should start from 1`` () =
-    generatePowersOfTwo 0 5 |> should equal [1.0; 2.0; 4.0; 8.0; 16.0; 32.0]
+    generatePowersOfTwo 0 5 |> should equal (Ok [1.0; 2.0; 4.0; 8.0; 16.0; 32.0])
 
 [<Test>]
 let ``generatePowersOfTwo 3 4 should return powers from 8 to 128`` () =
-    generatePowersOfTwo 3 4 |> should equal [8.0; 16.0; 32.0; 64.0; 128.0]
+    generatePowersOfTwo 3 4 |> should equal (Ok [8.0; 16.0; 32.0; 64.0; 128.0])
 
 [<Test>]
 let ``generatePowersOfTwo 1 0 should return single element 2`` () =
-    generatePowersOfTwo 1 0 |> should equal [2.0]
+    generatePowersOfTwo 1 0 |> should equal (Ok [2.0])
 
 [<Test>]
 let ``generatePowersOfTwo -1 3 should handle negative n`` () =
-    generatePowersOfTwo -1 3 |> should equal [0.5; 1.0; 2.0; 4.0]
+    generatePowersOfTwo -1 3 |> should equal (Ok [0.5; 1.0; 2.0; 4.0])
+
+[<Test>]
+let ``generatePowersOfTwo with negative m should return Error`` () =
+    generatePowersOfTwo 2 -3 |> should equal (Error "m не может быть отрицательным")
     
 [<Test>]
 let ``firstOccurrence in empty list should be None`` () =
