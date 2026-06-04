276ca246a647a972963277b00ef7ecfd73d2e25c
bogdanovaarina <arinabogdanova1967@mail.ru>
2026-06-05 00:42:59 +0300

    add проверка probability > 0.0 и завершение симуляции если нет кандидатов на заражение

--- HW5/HW5.Simulation/HW5.Simulation/Network.fs
+++ HW5/HW5.Simulation/HW5.Simulation/Network.fs
@@ -9,15 +9,15 @@ open HW5.Simulation.SimulationResult
 
 type Network (computers: Comp[], matrix: bool[,], virus: Virus, log: string -> unit, random: unit -> float) =
     let mutable currentInfected: int list = []
-    let mutable countTurn = 0
+    let mutable turnCount = 0
     
     member private n.GetCompById(id: int) : Comp =
         computers |> Array.find (fun c -> c.Id = id)
     
     member private n.GetNeighbors(id: int) : int list =
-        [0 .. computers.Length - 1] |> List.filter( fun i -> matrix[id, i])
+        [0 .. computers.Length - 1] |> List.filter(fun i -> matrix[id, i])
         
-    member private n.TryInjectComp(id: int) : bool =
+    member private n.TryInfectComp(id: int) : bool =
         let comp = n.GetCompById(id)
         let probability = virus.GetInfectionProbability(comp.Os)
         let roll = random()
@@ -29,7 +29,7 @@ type Network (computers: Comp[], matrix: bool[,], virus: Virus, log: string -> u
     member n.InitializePatientZero(patientZeroId : int) =
         let compPatient = n.GetCompById(patientZeroId)
         compPatient.Infect() |> ignore
-        countTurn <- 0
+        turnCount <- 0
         currentInfected <- [patientZeroId]
     
     member private n.PrintNetwork()=
@@ -47,7 +47,7 @@ type Network (computers: Comp[], matrix: bool[,], virus: Virus, log: string -> u
         let mutable hasCandidates = true
         
         while hasCandidates do
-            countTurn <- countTurn + 1
+            turnCount <- turnCount + 1
             let mutable candidatesForInfection = []
             
             let infectedToProcess = List.rev currentInfected
@@ -57,34 +57,46 @@ type Network (computers: Comp[], matrix: bool[,], virus: Virus, log: string -> u
                 for neighbor in neighbors do
                     let compNeighbor = n.GetCompById(neighbor)
                     if not compNeighbor.IsInfected then
-                        candidatesForInfection <- neighbor :: candidatesForInfection
+                        let probability = virus.GetInfectionProbability(compNeighbor.Os)
+                        if probability > 0.0 then
+                            candidatesForInfection <- neighbor :: candidatesForInfection
             
             let uniqueCandidates = candidatesForInfection |> List.distinct
             
-            let actualNewInfected =
-                uniqueCandidates |> List.filter (fun id -> n.TryInjectComp(id))
-            
-            log $"=== ХОД %d{countTurn} ==="
-
-            if actualNewInfected.IsEmpty then
-                log "Новых заражений нет."
+            if uniqueCandidates.IsEmpty then
+                log $"=== ХОД %d{turnCount} ==="
+                log "Нет кандидатов для заражения."
                 log "Симуляция завершена."
                 log "Итоговое состояние сети:"
                 n.PrintNetwork()
-                log $"Всего ходов: %d{countTurn}"
+                log $"Всего ходов: %d{turnCount}"
                 log $"Всего заражено компьютеров: %d{currentInfected.Length}"
                 hasCandidates <- false
             else
-                log (sprintf "Заразились в этом ходу: %s" 
-                    (actualNewInfected |> List.map (fun id -> $"PC-%d{id}") |> String.concat ", "))
+                let actualNewInfected =
+                    uniqueCandidates |> List.filter (fun id -> n.TryInfectComp(id))
                 
-                currentInfected <- List.append currentInfected actualNewInfected
-                
-                log "Текущее состояние сети:"
-                n.PrintNetwork()
+                log $"=== ХОД %d{turnCount} ==="
+
+                if actualNewInfected.IsEmpty then
+                    log "Новых заражений нет."
+                    log "Симуляция завершена."
+                    log "Итоговое состояние сети:"
+                    n.PrintNetwork()
+                    log $"Всего ходов: %d{turnCount}"
+                    log $"Всего заражено компьютеров: %d{currentInfected.Length}"
+                    hasCandidates <- false
+                else
+                    log (sprintf "Заразились в этом ходу: %s" 
+                        (actualNewInfected |> List.map (fun id -> $"PC-%d{id}") |> String.concat ", "))
+                    
+                    currentInfected <- List.append currentInfected actualNewInfected
+                    
+                    log "Текущее состояние сети:"
+                    n.PrintNetwork()
         
         {
-            TotalTurns = countTurn
+            TotalTurns = turnCount
             TotalInfected = currentInfected.Length
             Logs = []
         }
